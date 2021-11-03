bcbfc045b0283b71fe15b69a55fb68283267cd14
eden.ilan <eden.ilan@spot.io>
2021-11-03 11:52:58 +0200

    navbar: update @angular/cli (v12)

--- navbar/src/app/primary-nav/primary-nav.component.spec.ts
+++ navbar/src/app/primary-nav/primary-nav.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PrimaryNavComponent } from './primary-nav.component';
 
@@ -6,7 +6,7 @@ describe('PrimaryNavComponent', () => {
   let component: PrimaryNavComponent;
   let fixture: ComponentFixture<PrimaryNavComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PrimaryNavComponent ]
     })
